a6a779ae7a66a998158eec303df2002d38c46949
gbenini <gb.benini@gmail,com>
2026-09-22 00:40:47 -0300

    menu

---
+++ dap-kong/cenas/menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var label_2: Label = $VBoxContainer/Label2
+@onready var button_easy: Button = $VBoxContainer/ButtonEasy
+@onready var button_hard: Button = $VBoxContainer/ButtonHard
+
+
+func _ready() -> void:
+	button_easy.pressed.connect(_on_button_easy_pressed)
+	button_hard.pressed.connect(_on_button_hard_pressed)
+	
+	_atualizar_label()
+
+
+func _on_button_easy_pressed() -> void:
+	Global.modo = false
+	_atualizar_label()
+
+
+func _on_button_hard_pressed() -> void:
+	Global.modo = true
+	_atualizar_label()
+
+
+func _atualizar_label() -> void:
+	if Global.modo:
+		label_2.text = "Hard"
+		label_2.label_settings.font_color = Color("a30704")
+	else:
+		label_2.text = "Easy"
+		label_2.label_settings.font_color = Color.WHITE
